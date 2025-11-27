c694f54fec1f73c0d8368abafcc86edf4aa3cf95
devanshumankar <devanshumankar@gmail.com>
2025-11-27 16:04:41 +0530

    stopPropogation vs stopImmediatePropogation,target,currentTarget,call,bind and apply

---
+++ Lecture44-event delegation and dynamic content/script.js
@@ -0,0 +1,7 @@
+// problem:
+// not able to add event listeners dynamically
+// i.e container -> 5 div
+// on each div we will have event listeners 
+// this is the problem when we we have large data then memory will be used and adding 
+// changing  each  eventlistener is a difficult task
+
